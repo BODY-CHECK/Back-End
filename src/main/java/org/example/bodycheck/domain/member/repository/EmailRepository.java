349ca192c7c8911e2dd3e743a712fb0cdef45d6b
choyoungseo20 <whdudtj0809@naver.com>
2024-10-23 00:50:03 +0900

    이메일 인증코드 중복으로 저장되는 문제 해결

--- src/main/java/org/example/bodycheck/domain/member/repository/EmailRepository.java
+++ src/main/java/org/example/bodycheck/domain/member/repository/EmailRepository.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 
 public interface EmailRepository extends JpaRepository<Email, Long> {
 
+    boolean existsByEmail(String email);
     Optional<Email> findByEmail(String email);
 }
