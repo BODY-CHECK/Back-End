e7c3cb9acb29dd6ebdf1d2874da5d1bc42545a50
choyoungseo <127291939+choyoungseo20@users.noreply.github.com>
2024-11-03 17:27:24 +0900

    Revert "tts"

--- src/main/java/org/example/bodycheck/common/util/StringUtil.java
+++
@@ -1,7 +0,0 @@
-package org.example.bodycheck.common.util;
-
-import java.util.Objects;
-
-public class StringUtil {
-    public static boolean isNullOrBlank(String s) { return Objects.isNull(s) || s.isBlank(); }
-}
