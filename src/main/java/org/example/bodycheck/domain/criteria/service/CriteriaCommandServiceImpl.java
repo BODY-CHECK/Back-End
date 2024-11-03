4286ffc4e3a21b516aeb92063ff1124f827a331b
choyoungseo20 <whdudtj0809@naver.com>
2024-11-03 21:05:57 +0900

    Criteria 저장 방식 변경

--- src/main/java/org/example/bodycheck/domain/criteria/service/CriteriaCommandServiceImpl.java
+++ src/main/java/org/example/bodycheck/domain/criteria/service/CriteriaCommandServiceImpl.java
@@ -1,11 +1,33 @@
 package org.example.bodycheck.domain.criteria.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.bodycheck.domain.criteria.converter.CriteriaConverter;
+import org.example.bodycheck.domain.criteria.entity.Criteria;
+import org.example.bodycheck.domain.criteria.repository.CriteriaRepository;
+import org.example.bodycheck.domain.solution.dto.SolutionRequestDTO;
+import org.example.bodycheck.domain.solution.entity.Solution;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class CriteriaCommandServiceImpl implements CriteriaCommandService {
+    private final CriteriaRepository criteriaRepository;
+
+    @Override
+    @Transactional
+    public void saveCriteria(Solution solution, SolutionRequestDTO.SaveDTO request) {
+        if (solution.getCriteriaList() == null) {
+            solution.setCriteriaList(new ArrayList<>()); // `criteriaList`가 `null`이면 초기화
+        }
+        request.getCriteria().stream()
+                .forEach(criteriaItem -> {
+                    Criteria criteria = CriteriaConverter.toCriteria(criteriaItem);
+                    criteria.setSolution(solution);
+                    criteriaRepository.save(criteria);
+                });
+    }
 }
