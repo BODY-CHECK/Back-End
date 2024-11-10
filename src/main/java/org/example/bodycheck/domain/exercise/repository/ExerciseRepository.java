027472fac1828b75fb1ce46c191df3425922b817
KimChaeWon12 <kcw030220@naver.com>
2024-11-10 22:47:16 +0900

    add: randomRoutine

--- src/main/java/org/example/bodycheck/domain/exercise/repository/ExerciseRepository.java
+++ src/main/java/org/example/bodycheck/domain/exercise/repository/ExerciseRepository.java
@@ -1,10 +1,13 @@
 package org.example.bodycheck.domain.exercise.repository;
 
+import org.example.bodycheck.domain.enums.ExerciseType;
 import org.example.bodycheck.domain.exercise.entity.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
     Optional<Exercise> findByName(String name);
+    List<Exercise> findByType(ExerciseType type);
 }
