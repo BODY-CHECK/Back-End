025bcb68bcaea2cf3e2c3ee8ff3e2141d8f8a89c
choyoungseo20 <whdudtj0809@naver.com>
2024-11-17 15:25:43 +0900

    프롬프트 엔지니어링

--- src/main/java/org/example/bodycheck/domain/solution/service/SolutionCommandServiceImpl.java
+++ src/main/java/org/example/bodycheck/domain/solution/service/SolutionCommandServiceImpl.java
@@ -37,8 +37,20 @@ public class SolutionCommandServiceImpl implements SolutionCommandService {
 
         Exercise exercise = exerciseRepository.findById(exerciseId).orElseThrow(() -> new GeneralHandler(ErrorStatus.EXERCISE_NOT_FOUND));
 
-        String prompt = "운동 이름: " + exercise.getName() + "\n평가 지표 / 점수:\n" + criteriaText;
-        return prompt;
+        String prompt1 = "Your task is to respond in a consistent style. Answer in the given output format.\n"
+                + "input: \n"
+                + "Exercise Name: 푸쉬업\n"
+                + "Criteria / Score:\n"
+                + "1. 팔의 구부림 각도가 정확한가 / 100\n"
+                + "2. 자세가 일직선으로 정렬되어 있는가 / 50\n"
+                + "3. 무릎의 구부림 각도가 정확한가 / 75\n"
+                + "output:\n"
+                + "총점: 75 / 100\n"
+                + "팔을 잘 구부립니다! 하지만 푸쉬업을 할 때 허리를 곧게 피고, 무릎을 잘 구부리는 것이 중요하니, 신경써서 다시 시도해보세요!\n"
+                + "input: ";
+        String prompt2 = "Exercise Name: " + exercise.getName() + "\nCriteria / Score:\n" + criteriaText;
+        String prompt3 = "\noutput: ";
+        return prompt1 + prompt2 + prompt3;
     }
 
     @Override
