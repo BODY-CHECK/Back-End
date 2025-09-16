96757c3df316a251158a31c9f53ec2a8a957dd62
choyoungseo20 <whdudtj0809@naver.com>
2025-09-16 23:09:59 +0900

    일반 로그인 시 LoginType에 따른 에러 처리 추가

--- src/main/java/org/example/bodycheck/domain/member/dto/MemberDTO/MemberRequestDTO.java
+++ src/main/java/org/example/bodycheck/domain/member/dto/MemberDTO/MemberRequestDTO.java
@@ -30,6 +30,7 @@ public class MemberRequestDTO {
         @Email(message = "유효한 이메일 주소를 입력해주세요.")
         private String email;
         private String pw;
+        private LoginType loginType;
     }
 
     @Getter
