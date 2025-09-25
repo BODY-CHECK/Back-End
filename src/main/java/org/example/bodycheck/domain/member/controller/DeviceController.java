704d1403a4bd49b499fe422797bfa32f8cf023f5
choyoungseo20 <whdudtj0809@naver.com>
2025-09-25 16:19:44 +0900

    [REFACTOR] Device fcmToken 업데이트 API의 method를 POST에서 PUT으로 수정

--- src/main/java/org/example/bodycheck/domain/member/controller/DeviceController.java
+++ src/main/java/org/example/bodycheck/domain/member/controller/DeviceController.java
@@ -3,7 +3,7 @@ package org.example.bodycheck.domain.member.controller;
 import org.example.bodycheck.common.apipayload.ApiResponse;
 import org.example.bodycheck.domain.member.dto.devicedto.DeviceRequestDto;
 import org.example.bodycheck.domain.member.service.deviceservice.DeviceCommandService;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +17,7 @@ public class DeviceController {
 
 	private final DeviceCommandService deviceCommandService;
 
-	@PostMapping
+	@PutMapping
 	public ApiResponse<String> updateDeviceInfo(@RequestBody DeviceRequestDto.DeviceDto request) {
 		deviceCommandService.updateDevice(request);
 		return ApiResponse.onSuccess("OK");
